50429f8fb7c8ff3b1ef79e8b90c60040244a3635
msokolik55 <50205101+msokolik55@users.noreply.github.com>
2021-04-28 22:54:33 +0200

    Testing updating service workers

--- src/App.js
+++ src/App.js
@@ -389,7 +389,7 @@ function App() {
 
 	return (
 		<div className="container-fluid">
-			<div className="row text-center text-light my-6">
+			<div className="row text-center text-light my-1">
 				<h1>Zostavy</h1>
 
 				<div className="row">
